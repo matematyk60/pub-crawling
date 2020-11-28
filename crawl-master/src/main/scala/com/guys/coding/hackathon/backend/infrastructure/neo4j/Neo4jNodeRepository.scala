c3d5929e70f223419c7c3d7f5df934de6cc2f44d
Jakub Darul <daruljakub@protonmail.com>
2020-11-28 09:27:20 +0100

    add note

--- crawl-master/src/main/scala/com/guys/coding/hackathon/backend/infrastructure/neo4j/Neo4jNodeRepository.scala
+++ crawl-master/src/main/scala/com/guys/coding/hackathon/backend/infrastructure/neo4j/Neo4jNodeRepository.scala
@@ -7,6 +7,9 @@ import neotypes.implicits.syntax.all._
 
 class Neo4jNodeRepository(session: Session[IO]) {
 
+  // https://neotypes.github.io/neotypes/
+  // NOTE: neotypes.generic is scheduled for 0.16.0, now use  neotypes.implicits.mappers.all._ instead.
+
   def getPepole(): IO[List[(String, Int)]] = "match (p:Person) return p.name, p.born limit 10".query[(String, Int)].list(session)
 
 }
