ad96546d44ec0db44f0e2ca519489560be56c80a
Jakub Darul <daruljakub@protonmail.com>
2020-11-28 15:19:04 +0100

    sync

--- crawl-master/src/main/scala/com/guys/coding/hackathon/backend/domain/domain.scala
+++ crawl-master/src/main/scala/com/guys/coding/hackathon/backend/domain/domain.scala
@@ -2,3 +2,4 @@ package com.guys.coding.hackathon.backend.domain
 
 case class JobId(value: String)     extends AnyVal
 case class RequestId(value: String) extends AnyVal
+case class EntityValue(value: String) extends AnyVal
--- crawl-master/src/main/scala/com/guys/coding/hackathon/backend/infrastructure/neo4j/Neo4jNodeRepository.scala
+++ crawl-master/src/main/scala/com/guys/coding/hackathon/backend/infrastructure/neo4j/Neo4jNodeRepository.scala
@@ -4,12 +4,27 @@ import cats.effect.IO
 import neotypes.Session
 import neotypes.implicits.mappers.all._
 import neotypes.implicits.syntax.all._
+import com.guys.coding.hackathon.backend.domain.RequestId
+import com.guys.coding.hackathon.backend.domain.JobId
 
 class Neo4jNodeRepository(session: Session[IO]) {
 
+  // TODO:bcm  connect entity to request
   // https://neotypes.github.io/neotypes/
   // NOTE: neotypes.generic is scheduled for 0.16.0, now use  neotypes.implicits.mappers.all._ instead.
 
+
+  //
+
+  def save(requestId: RequestId) = ???
+
   def getPepole(): IO[List[(String, Int)]] = "match (p:Person) return p.name, p.born limit 10".query[(String, Int)].list(session)
+
+  case class Entity(
+      entityId: String, // TODO:bcm entity type
+      request: Option[RequestId]
+  )
+
+
 
 }
